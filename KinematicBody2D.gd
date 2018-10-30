2a896892d5046b8f06d07e9970c1dee22786ffe4
Tristan MacCrehan <trismac1@umbc.edu>
2018-10-30 19:02:27 -0400

    work submit

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -3,12 +3,12 @@ extends KinematicBody2D
 export (int) var speed = 0
 export (float) var rotation_speed = 1.5
 var force = Vector2()
-var velocity = Vector2()
+var velocity = Vector2(100,100)
 var acceleration = Vector2(0,0)
 var rotation_dir = 0
 var drag = 0.008
 var thrust = 100
-var mass = 0.01
+var mass = 0.001
 func _ready():
 	position = Vector2(300,300)
 	
@@ -22,7 +22,7 @@ func get_input():
 	if Input.is_action_pressed('ui_up'):
 		speed = thrust
 	else :
-		speed = 0
+		speed = velocity
 	if Input.is_action_pressed('ui_down'):
 		drag = 0.9
 
@@ -40,9 +40,9 @@ func checkwrap():
 func _physics_process(delta):
 	get_input()
 	rotation += rotation_dir * rotation_speed * delta
-	force = Vector2(speed, 0).rotated(rotation)
+	#Vector2(speed, 0).rotated(rotation)
 	acceleration = force / mass
-	velocity += acceleration * delta
-	velocity *= 1.0 - drag
-	move_and_slide(velocity)
+	velocity  = velocity + acceleration
+	velocity *= 0.5
+	move_and_slide(velocity * delta)
 	checkwrap()
